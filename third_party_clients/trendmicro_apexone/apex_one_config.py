e99b910560b9d404f232b9dcad4dcbc3264c8291
Matt Pieklik <mp@vectra.ai>
2022-12-20 08:48:31 -0600

    removed test credentials

--- third_party_clients/trendmicro_apexone/apex_one_config.py
+++ third_party_clients/trendmicro_apexone/apex_one_config.py
@@ -1,7 +1,7 @@
 # Connection information
-BASE_URL = 'https://bxhikz.manage.trendmicro.com'
-APPLICATION_ID = '56C1EC85-F2AF-4046-A2E8-B232053DE575'
-API_KEY = '722C2CF3-F7EB-4BE2-8057-526A48D8A891'
+BASE_URL = 'https://<update>.manage.trendmicro.com'
+APPLICATION_ID = '<update>'
+API_KEY = '<update>'
 
 # API Path - shouldn't need to change
 API_PATH = '/WebApp/API/AgentResource/ProductAgents'

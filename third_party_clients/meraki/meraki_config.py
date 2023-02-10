af6a1ece9246e713de670417c78da0c2635ebe2b
Matt Pieklik <mp@vectra.ai>
2023-02-10 16:24:18 -0600

    Updates to meraki

--- third_party_clients/meraki/meraki_config.py
+++ third_party_clients/meraki/meraki_config.py
@@ -1,5 +1,5 @@
 MERAKI_URL = 'https://api.meraki.com/api/v1'
-API_KEY = '966469e4f3659d85dd7dcdf08048162f8b51da6f'
+API_KEY = ''
 # Define to use a custom Group Policy vs default 'Blocked'
 BLOCK_GROUP_POLICY = ''
 VERIFY = False

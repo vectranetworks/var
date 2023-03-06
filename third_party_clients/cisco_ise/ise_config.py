f22bd3dfaa5b0e12d98c2db4935c91d2a9674771
Matt Pieklik <mp@vectra.ai>
2023-03-06 11:31:00 -0600

    Config file update for Cisco ISE

--- third_party_clients/cisco_ise/ise_config.py
+++ third_party_clients/cisco_ise/ise_config.py
@@ -5,3 +5,4 @@ CHECK_SSL = False
 # We need first to put the endpoint in a temporary policy to make the port bounce
 PORTBOUNCE_POLICY = 'PortBounce'
 QUARANTAINE_POLICY = 'Quarantine'
+ENHANCED = False
